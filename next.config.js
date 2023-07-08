cbda065f7260e41b3bc1114e84d32595bd9b4176
Pedro Silva <pedro_rike@hotmail.com.br>
2023-07-08 15:57:55 -0300

    feat: add trip item component

--- next.config.js
+++ next.config.js
@@ -1,14 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        port: '',
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
       },
     ],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ src/components/TripItem.tsx
@@ -0,0 +1,40 @@
+import { Trip } from "@prisma/client";
+import Image from "next/image";
+import React from "react";
+import ReactCountryFlag from "react-country-flag";
+
+interface TripItemProps {
+  trip: Trip;
+}
+
+function TripItem({ trip }: TripItemProps) {
+  return (
+    <div className="flex flex-col">
+      <div className="relative h-[280px] w-[280px]">
+        <Image
+          src={trip.coverImage}
+          fill
+          className="rounded-lg shadow-md"
+          alt={trip.name}
+          style={{
+            objectFit: "cover",
+          }}
+        />
+      </div>
+
+      <h3 className="text-primaryDarker font-medium font-sm mt-2">
+        {trip.name}
+      </h3>
+      <div className="flex items-center gap-1 my-1">
+        <ReactCountryFlag countryCode={trip.countryCode} svg />
+        <p className="text-xs text-grayPrimary">{trip.location}</p>
+      </div>
+      <p className="text-xs text-rayPrimary">
+        <span className="text-primary">R$ {trip.pricePerDay.toString()} </span>
+        por dia
+      </p>
+    </div>
+  );
+}
+
+export default TripItem;
