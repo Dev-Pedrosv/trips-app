8d07d8af31e1fc3b2a8dfe1e9b7433db9bb55cf9
Pedro Silva <pedro_rike@hotmail.com.br>
2023-07-07 23:07:12 -0300

    feat: add input datepicker and currency input components

---
+++ src/components/Header.tsx
@@ -0,0 +1,63 @@
+"use client";
+import { signIn, signOut, useSession } from "next-auth/react";
+import Image from "next/image";
+import React, { useState } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
+
+function Header() {
+  const { status, data } = useSession();
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  const handleLoginClick = () => signIn();
+
+  const handleMenuClick = () => setMenuIsOpen(!menuIsOpen);
+
+  const handleLogoutClick = () => {
+    setMenuIsOpen(false);
+    signOut();
+  };
+
+  return (
+    <div className="container mx-auto p-5 py-0 h-[93px] flex justify-between items-center">
+      <div className="relative h-[32px] w-[182px]">
+        <Image fill src="/logo.svg" alt="logo" />
+      </div>
+      {status === "unauthenticated" && (
+        <button
+          className="text-primary text-sm font-semibold"
+          onClick={handleLoginClick}
+        >
+          Login
+        </button>
+      )}
+      {status === "authenticated" && data.user && (
+        <div className="flex items-center gap-3 border-grayLighter p-2 px-3 border border-solid rounded-full relative">
+          <AiOutlineMenu
+            size={16}
+            onClick={handleMenuClick}
+            className="cursor-pointer"
+          />
+          <Image
+            className="rounded-full shadow-md"
+            height={35}
+            width={35}
+            alt={data.user!.name!}
+            src={data.user.image!}
+          />
+          {menuIsOpen && (
+            <div className="absolute top-12 left-0 w-full h-full bg-white rounded-lg shadow-md flex flex-col justify-center items-center ">
+              <button
+                className="text-primary text-sm font-semibold"
+                onClick={handleLogoutClick}
+              >
+                Logout
+              </button>
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Header;
