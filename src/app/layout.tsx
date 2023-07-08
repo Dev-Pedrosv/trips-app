448a5562d3408c1550d95776efae773e5e361ff5
Pedro Silva <pedro_rike@hotmail.com.br>
2023-07-07 23:11:54 -0300

    feat: add initial search trip

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { NextAuthProvider } from "@/providers/auth";
 import "./globals.css";
 import { Poppins } from "next/font/google";
+import Header from "@/components/Header";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -20,7 +21,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className}>
-        <NextAuthProvider>{children}</NextAuthProvider>
+        <NextAuthProvider>
+          <Header />
+          {children}
+        </NextAuthProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 "use client";
 
+import TripSearch from "./components/TripSearch";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="">
+      <TripSearch />
+    </div>
+  );
 }
