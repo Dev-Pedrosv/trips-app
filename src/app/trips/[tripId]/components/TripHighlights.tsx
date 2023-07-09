5b8043520fa99de90e09e07e8ae24d7f696cf854
Pedro Silva <pedro_rike@hotmail.com.br>
2023-07-09 17:27:10 -0300

    style: addapt trips details to desktop

--- src/app/trips/[tripId]/components/TripHighlights.tsx
+++ src/app/trips/[tripId]/components/TripHighlights.tsx
@@ -7,18 +7,23 @@ interface TripHighlightsProps {
 
 function TripHighlights({ highlights }: TripHighlightsProps) {
   return (
-    <div className="flex flex-col p-5">
-      <h2 className="font-semibold text-primaryDarker mb-2">Destaques</h2>
-      <div className="flex flex-wrap gap-y-3">
+    <div className="flex flex-col p-5 lg:p-0 lg:mt-12">
+      <h2 className="font-semibold text-primaryDarker mb-2 lg:text-xl">
+        Destaques
+      </h2>
+      <div className="flex flex-wrap gap-y-3 lg:mt-5">
         {highlights.map((highlight) => (
-          <div className="flex items-center gap-2 w-1/2" key={highlight}>
+          <div
+            className="flex items-center gap-2 w-1/2 lg:gap-3"
+            key={highlight}
+          >
             <Image
               src="/check-icon.png"
               alt={highlight}
               width={15}
               height={15}
             />
-            <p className="text-xs text-grayPrimary">{highlight}</p>
+            <p className="text-xs text-grayPrimary lg:text-base">{highlight}</p>
           </div>
         ))}
       </div>
--- src/app/trips/[tripId]/components/TripLocation.tsx
+++ src/app/trips/[tripId]/components/TripLocation.tsx
@@ -9,10 +9,15 @@ interface TripLocationProps {
 
 function TripLocation({ location, locationDescription }: TripLocationProps) {
   return (
-    <div className="p-5">
-      <h2 className="font-semibold text-primaryDarker mb-5">Localização</h2>
+    <div className="p-5 lg:p-0 lg:mt-12 lg:pb-20">
+      <h2 className="font-semibold text-primaryDarker mb-5 lg:text-xl">
+        Localização
+      </h2>
 
-      <div className="relative w-full h-[280px]" style={{ objectFit: "cover" }}>
+      <div
+        className="relative w-full h-[280px] lg:hidden"
+        style={{ objectFit: "cover" }}
+      >
         <Image
           src="/map-mobile.png"
           alt={location}
@@ -21,10 +26,22 @@ function TripLocation({ location, locationDescription }: TripLocationProps) {
         />
       </div>
 
-      <h3 className="text-sm font-semibold text-primaryDarker mt-3">
+      <div
+        className="hidden lg:block relative w-full h-[480px]"
+        style={{ objectFit: "cover" }}
+      >
+        <Image
+          src="/map-desktop.png"
+          alt={location}
+          fill
+          className="rounded-lg shadow-md"
+        />
+      </div>
+
+      <h3 className="text-sm font-semibold text-primaryDarker mt-3 lg:text-base lg:mt-5">
         {location}
       </h3>
-      <p className="text-xs text-primaryDarker mt-3 leading-5">
+      <p className="text-xs text-primaryDarker mt-3 leading-5 lg:text-sm lg:mt-4">
         {locationDescription}
       </p>
       <Button variant="outlined" className="w-full mt-5">
