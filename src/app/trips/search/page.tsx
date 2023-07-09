32d8389b080ae17a1b8698d829e49134d00f2e02
Pedro Silva <pedro_rike@hotmail.com.br>
2023-07-09 17:10:22 -0300

    style: addapt search page to desktop

--- src/app/trips/search/page.tsx
+++ src/app/trips/search/page.tsx
@@ -26,16 +26,16 @@ function TripsSearch() {
   }, []);
 
   return (
-    <div className="container mx-auto flex flex-col p-5 items-center">
-      <h1 className="text-primaryDarker font-semibold text-xl">
+    <div className="container mx-auto flex flex-col p-5 items-start">
+      <h1 className="text-primaryDarker font-semibold text-xl lg:text-[2.5rem] lg:w-full lg:text-left">
         Viagens encontradas
       </h1>
-      <h2 className="text-grayLighter font-medium mb-5">
+      <h2 className="text-grayLighter font-medium mb-5 lg:mt-6 lg:w-full lg:text-left">
         {trips && trips?.length > 0
           ? "Listamos as melhores viagens para você!"
           : "Não encontramos nenhuma viagem"}
       </h2>
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4 lg:grid lg:grid-cols-4 lg:mt-6 lg:pb-16 lg:gap-10 ">
         {trips?.map((trip) => (
           <TripItem trip={trip} key={trip.id} />
         ))}
