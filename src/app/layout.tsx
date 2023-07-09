a1b1047acbe61048278506ff7455eab3c32d6ae7
Pedro Silva <pedro_rike@hotmail.com.br>
2023-07-09 15:06:39 -0300

    style: footer style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,9 +25,13 @@ export default function RootLayout({
       <body className={poppins.className}>
         <NextAuthProvider>
           <ToastProvider>
-            <Header />
-            {children}
-            <Footer />
+            <div className="flex flex-col h-screen">
+              <div className="h-[94px]">
+                <Header />
+              </div>
+              <div className="flex-1">{children}</div>
+              <Footer />
+            </div>
           </ToastProvider>
         </NextAuthProvider>
       </body>
